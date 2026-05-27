b193d3aaaa1ecaecd45a53df982a28902895e9a9
ELIS JASPER <elis_jasper@estudante.sesisenai.org.br>
2026-05-27 15:21:51 -0300

    [FRONT-13] feat/finaliza estrutura inicial da tela inicial

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -5,7 +5,7 @@ export default function SistemaLayout({ children }) {
     return (
         <div className="h-screen flex overflow-hidden">
             <Sidebar />
-            <main className="flex-1 flex mt-28 mx-20 flex-col min-w-0 mt-5">
+            <main className="flex-1 flex mt-28 mx-20 flex-col min-w-0 mt-5 mb-29">
                 {children}
             </main>
         </div>
