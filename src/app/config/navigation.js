475fedb0efe3c6ce048b316e08cbc1d8841babef
EMANUELLE CRISTINA HOSTIN <emanuelle_hostin@estudante.sesisenai.org.br>
2026-05-27 19:55:01 -0300

    [FRONT-17] feat/implementa tela de cadastro de usuários para o usuário administrador.

--- src/app/config/navigation.js
+++ src/app/config/navigation.js
@@ -25,3 +25,31 @@ export const sidebarRoutes = [
         icon: "/images/config.png",
     },
 ]
+
+export const admRoutes = [
+    {
+        label: "Início",
+        href: "/",
+        icon: "/images/home.png",
+    },
+    {
+        label: "Gerenciar Usuários",
+        href: "/usuarios",
+        icon: "/images/gerenciarUsuarios.png",
+    },
+    {
+        label: "Análise de Registros",
+        href: "/usuarios/cadastro",
+        icon: "/images/analiseRegistros.png",
+    },
+    {
+        label: "Notificações",
+        href: "/notificacoes",
+        icon: "/images/notificacoes.png",
+    },
+    {
+        label: "Configurações",
+        href: "/configuracoes",
+        icon: "/images/config.png",
+    },
+]
