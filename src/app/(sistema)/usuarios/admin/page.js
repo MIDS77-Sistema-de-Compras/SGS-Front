b90c89da1a95bde45f2416085e1b175a3b199e11
Elis <elis_jasper@estudante.sesisenai.org.br>
2026-05-29 15:20:26 -0300

    [FRONT-17] feat/adiciona força de senha funcional e corrige img para Image

--- src/app/(sistema)/usuarios/admin/page.js
+++ src/app/(sistema)/usuarios/admin/page.js
@@ -1,11 +1,14 @@
 'use client';
+
 import { useState } from 'react';
+import Image from 'next/image';
+import PasswordField from '@/components/adm/PasswordField';
 
 export default function CadastroUsuarios() {
   const [nivelAcesso, setNivelAcesso] = useState('DOCENTE');
 
   return (
-    <div className="w-full h-full pb-12 flex flex-col">
+    <div className="w-full flex flex-col">
 
       <div className="bg-white p-5 rounded-xl shadow-sm border border-[#AAAAAA] flex flex-col justify-between">
 
@@ -51,9 +54,12 @@ export default function CadastroUsuarios() {
                   <div className="flex rounded-xl border border-gray-200 shadow-sm overflow-hidden focus-within:ring-1 focus-within:ring-blue-500">
 
                     <div className="bg-gray-50 px-3 flex items-center justify-center border-r border-gray-200 text-gray-400 text-sm">
-                      <img
+                      <Image
                         src="/images/adm/telefone.png"
                         alt="Ícone de nível de acesso"
+                        width={4}
+                        height={4}
+                        className='w-auto h-auto'
                       />
                     </div>
 
@@ -73,9 +79,12 @@ export default function CadastroUsuarios() {
                   <div className="flex rounded-xl border border-gray-200 shadow-sm overflow-hidden focus-within:ring-1 focus-within:ring-blue-500">
 
                     <div className="bg-gray-50 px-3 flex items-center justify-center border-r border-gray-200 text-gray-400 text-sm">
-                      <img
+                      <Image
                         src="/images/adm/telefone.png"
                         alt="Ícone de nível de acesso"
+                        width={4}
+                        height={4}
+                        className='w-auto h-auto'
                       />
                     </div>
 
@@ -99,28 +108,7 @@ export default function CadastroUsuarios() {
                   />
                 </div>
 
-                <div className="md:col-span-2">
-                  <label className="block text-[12px] font-bold text-[#103D85]/70 mb-2">
-                    Senha <span className="text-[#BA1A1A]">*</span>
-                  </label>
-
-                  <input
-                    type="password"
-                    placeholder="Mínimo 8 caracteres"
-                    className="w-full px-3 py-2.5 border border-gray-200 shadow-sm rounded-xl text-sm placeholder-[#6B7280]"
-                  />
-
-                  <div className="flex gap-3 pt-5">
-                    <div className="h-2 w-1/4 bg-[#103D85] rounded"></div>
-                    <div className="h-2 w-1/4 bg-[#103D85] rounded"></div>
-                    <div className="h-2 w-1/4 bg-[#103D85] rounded"></div>
-                    <div className="h-2 w-1/4 bg-gray-200 rounded"></div>
-                  </div>
-
-                  <span className="text-[10px] text-gray-500 block pt-2">
-                    Nível de força
-                  </span>
-                </div>
+                <PasswordField />
 
               </div>
             </div>
@@ -135,7 +123,7 @@ export default function CadastroUsuarios() {
                 </h2>
               </div>
 
-              <div className="grid grid-cols-2 md:grid-cols-4 gap-3 pt-4">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-7 mb-2">
 
                 {['COORDENADOR', 'SUPERVISOR', 'DOCENTE', 'COMPRADOR REG.'].map((nivel) => (
 
@@ -144,17 +132,19 @@ export default function CadastroUsuarios() {
                     type="button"
                     onClick={() => setNivelAcesso(nivel)}
                     className={`py-3 px-4 rounded-xl border text-xs transition-all flex items-center justify-center gap-2 shadow-sm ${nivelAcesso === nivel
-                        ? 'bg-[#103D85] text-white border-[#103D85]'
-                        : 'bg-white text-[#103D85] border-gray-200 hover:bg-gray-100'
+                      ? 'bg-[#103D85] text-white border-[#103D85]'
+                      : 'bg-white text-[#103D85] border-gray-200 hover:bg-gray-100'
                       }`}
                   >
 
-                    <img
+                    <Image
                       src="/images/adm/usuario.png"
                       alt="Ícone de nível de acesso"
-                      className={`w-4 h-4 duration-200 ${nivelAcesso === nivel
-                          ? 'brightness-0 invert'
-                          : ''
+                      width={10}
+                      height={10}
+                      className={`w-auto h-auto duration-200 ${nivelAcesso === nivel
+                        ? 'brightness-0 invert'
+                        : ''
                         }`}
                     />
 
@@ -169,21 +159,16 @@ export default function CadastroUsuarios() {
           </form>
         </div>
 
-
-
       </div>
 
-      <div className="flex justify-end pt-4">
-
+      <div className="flex justify-end mt-5">
         <button
           type="submit"
-          className="bg-[#103D85] hover:bg-[#0b2a5c] text-white font-bold text-[14px] py-3 px-27 rounded-xl transform translate-y-[30px]"
+          className="bg-[#103D85] hover:bg-[#0b2a5c] text-white shadow-sm font-bold text-[14px] py-3 px-27 rounded-xl transform"
         >
           Criar Usuário +
         </button>
-
       </div>
-
     </div>
   );
 }
