0e4e5073e29fde0cfaa0d03c4b3e2b003aefe17e
EMANUELLE CRISTINA HOSTIN <emanuelle_hostin@estudante.sesisenai.org.br>
2026-06-10 19:08:22 -0300

    [FRONT-45] fix/corrige TopBar e SideBar da tela minhas solicitações.

--- src/app/(sistema)/solicitacoes/page.js
+++ src/app/(sistema)/solicitacoes/page.js
@@ -106,8 +106,7 @@ export default function TodasSolicitacoes() {
     ];
 
     return (
-        <div className="absolute inset-y-0 right-0 w-[1500px] left-[260px] bg-[#FFFFFF] px-12 pb-12 overflow-y-auto font-sans flex flex-col gap-6">
-            <TopBar />
+        <div className="flex-1 bg-white pb-12 overflow-y-auto font-sans flex flex-col gap-6">
 
             {/* Filtros */}
             <div className="bg-white rounded-xl border border-[#797979] flex flex-row justify-between p-2">
