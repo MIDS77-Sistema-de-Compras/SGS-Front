b953125c95b2f59126f31107e5ee8c716f94d8f0
GABRIELLI VITÓRIA GLOWATSKI <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-07-17 20:24:12 -0300

    [FRONT-211] fix/ajusta-quebra-na-responsivadade-das-telas-de-monitoramento-e-nova-solicitacao

--- src/app/(sistema)/solicitacoes/criar/page.js
+++ src/app/(sistema)/solicitacoes/criar/page.js
@@ -7,13 +7,13 @@ export const metadata = {
 
 export default function NovaSolicitacao() {
     return (
-        <div className="flex flex-col xl:flex-row gap-10 xl:flex-1 xl:min-h-0">
+        <div className="scroll-native flex flex-col min-[1450px]:flex-row gap-6 min-[1450px]:gap-10 flex-1 min-h-0 overflow-y-auto min-[1450px]:overflow-visible pr-2 min-[1450px]:pr-0 max-[1449px]:pb-60">
 
-            <div className="flex flex-col xl:flex-1 xl:min-h-0">
+            <div className="flex flex-col min-[1450px]:flex-1 min-[1450px]:min-h-0">
                 <RequestForm />
             </div>
 
-            <div className="w-full xl:w-auto flex flex-col xl:min-h-0">
+            <div className="w-full min-[1450px]:w-auto flex flex-col shrink-0 min-[1450px]:min-h-0">
                 <Search />
             </div>
         </div>
