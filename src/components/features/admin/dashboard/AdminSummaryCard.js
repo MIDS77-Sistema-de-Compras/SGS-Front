24b1396202355c15496effa27148cf2972c5e9c9
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-07-20 19:56:28 -0300

    FRONT-280 feat/integra a página inicial do administrador com a API

--- src/components/features/admin/dashboard/AdminSummaryCard.js
+++ src/components/features/admin/dashboard/AdminSummaryCard.js
@@ -1,7 +1,7 @@
 import AdminSummaryItem from "./AdminSummaryItem";
 import AdminAlertItem from "./AdminAlertItem";
 
-export default function AdminSummaryCard({ title, items = [], alertCount }) {
+export default function AdminSummaryCard({ title, items = [] }) {
 
     return (
         <div className="min-w-0 flex-1 border border-gray-100 dark:border-white/10 rounded-xl px-4 sm:px-5 py-4 sm:py-3 shadow-sm dark:bg-[#1A2233]">
@@ -11,9 +11,9 @@ export default function AdminSummaryCard({ title, items = [], alertCount }) {
             <div className="border-t border-gray-100 dark:border-white/10 mt-2 -mx-4 sm:-mx-5" />
 
             <ul className="flex flex-col gap-5 mt-3">
-                <AdminAlertItem count={alertCount} />
-                <AdminSummaryItem {...items[0]} />
+                <AdminAlertItem count={items[0].count} />
                 <AdminSummaryItem {...items[1]} />
+                <AdminSummaryItem {...items[2]} />
             </ul>
         </div>
     );
